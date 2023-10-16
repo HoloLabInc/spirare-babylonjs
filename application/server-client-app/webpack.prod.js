05fcee71b8c6633da170ab05350c4f6e270ad74b
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2023-10-16 17:33:12 +0900

    update

--- application/server-client-app/webpack.prod.js
+++ application/server-client-app/webpack.prod.js
@@ -4,6 +4,7 @@ const [
   clientConfigCommon,
   eighthwallConfigCommon,
   startpageConfigCommon,
+  artoolkitConfigCommon,
 ] = require('./webpack.common.js')
 
 const serverConfig = merge(serverConfigCommon, {
@@ -22,4 +23,8 @@ const startpageConfig = merge(startpageConfigCommon, {
   mode: 'production',
 })
 
-module.exports = [serverConfig, clientConfig, eighthwallConfig, startpageConfig]
+const artoolkitConfig = merge(artoolkitConfigCommon, {
+  mode: 'production',
+})
+
+module.exports = [serverConfig, clientConfig, eighthwallConfig, startpageConfig, artoolkitConfig]
